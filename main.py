bfea20dcbc5981e2c91e5bf9a546e9ff8eac4581
Henrique Mayor <henriquessm.al.insper.edu.br>
2025-06-03 19:00:15 -0300

    fix

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from currency_converter import CurrencyConverter
 import time
 import requests
 
@@ -8,9 +7,6 @@ app = FastAPI()
 
 @app.get("/exchange/{f}/{to}")
 async def exchange(f, to):
-    c = CurrencyConverter()
-    sell = c.convert(1, f, to)
-    buy = c.convert(1, to, f)
     date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     route = f'https://economia.awesomeapi.com.br/json/last/{f}-{to}'
 
